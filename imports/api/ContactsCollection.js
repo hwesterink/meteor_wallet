cc54d91b91a978704d2e9ed1ffe1fcb5436caaec
Henk Westerink <h.p.westerink@kpnmail.nl>
2022-01-18 18:01:18 +0100

    First commit of this repository

---
+++ imports/api/ContactsCollection.js
@@ -0,0 +1,3 @@
+import { mongo } from "meteor/mongo";
+
+export const ContactsCollection = new Mongo.Collection("contacts");
---
+++ imports/ui/ContactForm.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {ContactsCollection} from "../api/ContactsCollection";
+
+export const ContactForm = () => {	
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+
+  const saveContact = () => {
+	ContactsCollection.insert({ name, email, imageUrl })
+	setName("");
+	setEmail("");
+	setImageUrl("");
+  }
+
+  return (
+    <form>
+      <div>
+	    <label htmlFor="name">Name</label>
+        <input id="name"
+               type="text"
+			   value={name}
+               onChange={(e) => setName(e.target.value)} />
+	  </div>
+      <div>
+	    <label htmlFor="email">Email</label>
+        <input id="email"
+		       type="email"
+			   value={email}
+               onChange={(e) => setEmail(e.target.value)} />
+	  </div>
+      <div>
+	    <label htmlFor="imageUrl">Image URL</label>
+        <input id="imageUrl"
+               type="text"
+			   value={imageUrl}
+               onChange={(e) => setImageUrl(e.target.value)} />
+	  </div>
+	  <div>
+	    <button type="button" onClick={saveContact}>Save Contact</button>
+	  </div>
+    </form>
+  )
+}
