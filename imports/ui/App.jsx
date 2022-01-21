898fef3d3dcffe63165b8d9f2e6a09d1dd055166
Henk Westerink <h.p.westerink@kpnmail.nl>
2022-01-21 18:40:54 +0100

    Included Tailwind in the user interface

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Header} from "./Header";
 import {ContactForm} from "./ContactForm";
 import {ContactList} from "./ContactList";
 
 // Our contact has a name, email, imageURL
 export const App = () => (
   <div>
-    <h1>Meteor Wallet - Galaxy</h1>
-	<ContactForm />
-	<ContactList />
+    <Header />
+	<div className="min-h-full">
+	  <div className="max-w-4xl mx-auto p-2">
+	    <ContactForm />
+	    <ContactList />
+	  </div>
+	</div>
   </div>
 );
--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -14,31 +14,56 @@ export const ContactForm = () => {
   }
 
   return (
-    <form>
-      <div>
-	    <label htmlFor="name">Name</label>
-        <input id="name"
-               type="text"
-			   value={name}
-               onChange={(e) => setName(e.target.value)} />
-	  </div>
-      <div>
-	    <label htmlFor="email">Email</label>
-        <input id="email"
-		       type="email"
-			   value={email}
-               onChange={(e) => setEmail(e.target.value)} />
-	  </div>
-      <div>
-	    <label htmlFor="imageUrl">Image URL</label>
-        <input id="imageUrl"
-               type="text"
-			   value={imageUrl}
-               onChange={(e) => setImageUrl(e.target.value)} />
-	  </div>
-	  <div>
-	    <button type="button" onClick={saveContact}>Save Contact</button>
-	  </div>
+    <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+      </div>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={saveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
+          Save Contact
+        </button>
+      </div>
     </form>
   )
 }
